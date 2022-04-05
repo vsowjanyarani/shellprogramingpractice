b17556a1ed7938a6a5d3c5f8f85be72af929b535
Ubuntu <ubuntu@ip-172-31-12-254.us-east-2.compute.internal>
2022-04-05 12:48:48 +0000

    file count function

---
+++ funFile-count.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+file-count()
+{
+ echo -n "$(ls)" | wc -l
+}
+file-count $1
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                           
