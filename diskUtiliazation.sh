685ee3c05965d099339d2419c31359588bcf5bd4
Ubuntu <ubuntu@ip-172-31-15-88.us-east-2.compute.internal>
2022-04-21 09:26:55 +0000

    disk utiliazation

--- diskUtiliazation.sh
+++ diskUtiliazation.sh
@@ -1,4 +1,4 @@
 
 diskinfo=$(df -H | grep /dev/root | awk '{print "disk usuage is " $5}')
 echo " disk info is= $diskinfo"
-curl -X POST -H 'Content-type: application/json' --data "{"text":\"${diskinfo}\"}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data "{"text":\"${diskinfo}\"}" https://hooks.slack.com/services/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
