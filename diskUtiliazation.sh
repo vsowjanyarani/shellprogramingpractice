d8271ee77d69b4184a6b654bd6dc77f02364d9ac
Ubuntu <ubuntu@ip-172-31-15-88.us-east-2.compute.internal>
2022-04-23 10:16:33 +0000

    commit changed

--- diskUtiliazation.sh
+++ diskUtiliazation.sh
@@ -1,4 +1,4 @@
 
 diskinfo=$(df -H | grep /dev/root | awk '{print "disk usuage is " $5}')
 echo " disk info is= $diskinfo"
-curl -X POST -H 'Content-type: application/json' --data "{"text":\"${diskinfo}\"}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data "{"text":\"${diskinfo}\"}" https://hooks.slack.com/services/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
--- tomcatcheck.sh
+++ tomcatcheck.sh
@@ -3,6 +3,6 @@
  if [ $n -eq 0 ]
    then
     echo " tomcat is not running"
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"tomcat is not running"}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"tomcat is not running"}' https://hooks.slack.com/services/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
     else   echo " tomcat is  running"
  fi
