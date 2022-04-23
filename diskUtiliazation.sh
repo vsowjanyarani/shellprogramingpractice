993b4d0d833d354f21d7e81c2ed18daef2a85361
vsowjanyarani <46196646+vsowjanyarani@users.noreply.github.com>
2022-04-23 15:59:17 +0530

    add new webhook

--- diskUtiliazation.sh
+++ diskUtiliazation.sh
@@ -1,4 +1,4 @@
 
 diskinfo=$(df -H | grep /dev/root | awk '{print "disk usuage is " $5}')
 echo " disk info is= $diskinfo"
-curl -X POST -H 'Content-type: application/json' --data "{"text":\"${diskinfo}\"}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data "{"text":\"${diskinfo}\"}" https://hooks.slack.com/services/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
