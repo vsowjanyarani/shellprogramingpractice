028201433b9489e7e06d6be1533d48840f7d4f65
Ubuntu <ubuntu@ip-172-31-15-88.us-east-2.compute.internal>
2022-04-23 09:56:54 +0000

    tomatcheck

--- tomcatcheck.sh
+++ tomcatcheck.sh
@@ -3,6 +3,6 @@
  if [ $n -eq 0 ]
    then
     echo " tomcat is not running"
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"tomcat is not running!"}' https://hooks.slack.com/services/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
-  else   echo " tomcat is  running"
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"tomcat is not running"}' https://hooks.slack.com/services/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
+    else   echo " tomcat is  running"
  fi
